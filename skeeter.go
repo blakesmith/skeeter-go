caf739dd74cda79ab33c57ac5ee1fdf413e11583
Blake Smith <blakesmith0@gmail.com>
2012-07-07 09:52:50 -0500

    Setup boilerplate to open an image

--- skeeter.go
+++ skeeter.go
@@ -3,35 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os/exec"
+	"image"
+	"os"
+	"strconv"
 )
 
-// Borrowed from: https://gist.github.com/1477401
-func pipe_commands(commands ...*exec.Cmd) ([]byte, error) {
-	for i, command := range commands[:len(commands)-1] {
-		out, err := command.StdoutPipe()
-		if err != nil {
-			return nil, err
-		}
-		command.Start()
-		commands[i+1].Stdin = out
-	}
-	final, err := commands[len(commands)-1].Output()
+import _ "image/png"
+import _ "image/jpeg"
+import _ "image/gif"
+
+func getImage(url string) image.Image {
+	fmt.Printf("Ignoring url %s\n", url)
+
+	file, err := os.Open("/Users/blake/projects/skeeter-go/images/moose-ascii.jpg")
 	if err != nil {
-		return nil, err
+		fmt.Println("Failed to open file!")
 	}
-	return final, nil
+
+	im, _, err := image.Decode(file)
+	
+	if err != nil {
+		fmt.Println("Failed to decode image!")
+	}
+
+	return im
+}
+
+func toAscii (img image.Image, width int) string {
+	return "00000000000"
 }
 
 func makeImage(url string, width string) string {
-	curl := exec.Command("curl", "-s", url)
-	convert := exec.Command("convert", "-", "jpg:-")
-	jp2a := exec.Command("jp2a", "-", fmt.Sprintf("--width=%s", width))
-
-	out, err := pipe_commands(curl, convert, jp2a)
+	img := getImage(url)
+	w, err := strconv.Atoi(width)
 	if err != nil {
-		fmt.Printf("Commands failed to run: %s\n", err)
+		fmt.Printf("Failed to convert width to an int!")
 	}
+	out := toAscii(img, w)
 
 	return string(out)
 }
@@ -62,6 +70,7 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Println("Running server...")
 	http.HandleFunc("/", imageHandler)
 	http.ListenAndServe(":9001", nil)
 }
